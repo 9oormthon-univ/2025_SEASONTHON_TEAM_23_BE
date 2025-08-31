92434b98bc10c87312f3ee510b4730d7abce6c3e
Eunbi <eunbi9533@naver.com>
2025-08-31 18:34:18 +0900

    feature: 사용자 정보 저장 및 조회 기능 구현

---
+++ src/main/java/com/petfarewell/auth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.petfarewell.auth.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.petfarewell.auth.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByKakaoId(Long kakaoId);
+}
+
