c18d932f8ecb523d98d49df8cfdd7b7367423e6b
Eunbi <eunbi9533@naver.com>
2025-08-31 18:35:13 +0900

    feature: 인증 토큰 DTO 작성

---
+++ src/main/java/com/petfarewell/auth/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,16 @@
+package com.petfarewell.auth.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Schema(description = "액세스 토큰 갱신 요청")
+public class RefreshTokenRequest {
+    @Schema(description = "리프레시 토큰", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String refreshToken;
+}
+
