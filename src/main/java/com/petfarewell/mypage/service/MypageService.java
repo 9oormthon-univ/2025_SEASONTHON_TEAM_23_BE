bd94b79e0b8350d90de9c2a25ba4bf62d42743e0
Eunbi <eunbi9533@naver.com>
2025-09-07 06:15:04 +0900

    fix: 내 활동 요약 집계 null 처리 안 되게 변경

--- src/main/java/com/petfarewell/mypage/service/MypageService.java
+++ src/main/java/com/petfarewell/mypage/service/MypageService.java
@@ -28,7 +28,7 @@ public class MypageService {
 
         long diaryCount = diaryRepository.countByUserAndDeletedFalse(user);
         long letterCount = letterRepository.countByUser(user);
-        long tributeCount = letterTributeRepository.getTotalTributeCountByUserId(user);
+        long tributeCount = letterTributeRepository.getTotalTributeCountByUser(user);
 
         return new UserActivitySummaryResponse(diaryCount, letterCount, tributeCount);
     }
